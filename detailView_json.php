c27ec786d025c9e78de8dcb849977d4aad7342db
Marco Bertini <marco.bertini@unifi.it>
2023-03-14 19:47:23 +0100

    NEW: new PHP error messages and logging

--- detailView_json.php
+++ detailView_json.php
@@ -9,15 +9,21 @@ $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 if (!$conn) {
     echo 'Connection error: ' . mysqli_connect_error();
 }
+/* change character set to utf8 | Object Oriented*/
+if (!$conn->set_charset("utf8")) {
+    error_log("Error loading character set utf8: %s\n", $conn->error);
+}
 $id = $conn->escape_string($id);
 $sql = "SELECT * FROM details_".$lang." WHERE id='$id'";
 
+error_log('SQL query: ' . $sql); // debugging
 $result = mysqli_query($conn, $sql);
 
 $detail = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 mysqli_close($conn);
 
+error_log('JSON details: ' . json_encode($detail, JSON_PARTIAL_OUTPUT_ON_ERROR));
 // convert php to json
 echo json_encode($detail);
 
